5d1f91d694593351322e4ad5b90868022ca50b8c
Kevin Shin <kevin.shin@patientco.com>
2019-03-06 07:29:48 -0500

    Adding Semantic UI

---
+++ src/views/CreateUser.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <button class="ui primary button">Primary</button>
+    <sui-button secondary>Secondary</sui-button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ButtonEmphasisExample',
+};
+</script>
